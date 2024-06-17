eb0aae050d46238429365cd9a2e454cfdc7fa1c8
joshnice <joshliamnice@gmail.com>
2024-06-17 10:13:23 +0100

    Fix cors issue

--- node/index.js
+++ node/index.js
@@ -6,6 +6,18 @@ const readFile = require("./file-helpers").readFile;
 const server = createServer(async (req, res) => {
   const pathname = url.parse(req.url).pathname;
 
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow any origin
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET"); // Allowed methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allowed headers
+
+  // Preflight request handling
+  if (req.method === "OPTIONS") {
+    res.writeHead(204); // No content
+    res.end();
+    return;
+  }
+
   switch (pathname) {
     case "/style":
       res.writeHead(200, { "Content-Type": "application/json" });
